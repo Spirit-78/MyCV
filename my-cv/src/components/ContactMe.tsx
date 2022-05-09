9dc73d7832ee39a9502cdce21272017873febd2a
ElenaPapazova <elena.papazova@miraclemill.com>
2022-05-09 20:20:51 +0200

    add contact me frame

--- my-cv/src/components/ContactMe.tsx
+++ my-cv/src/components/ContactMe.tsx
@@ -1,3 +1,24 @@
+import { Fieldset, Frame } from "@react95/core";
+
 export const ContactMe = () => {
-	return <></>;
+	return (
+		<Frame>
+			<Fieldset legend="Phone Number">
+				<Frame className="languageFrame">
+					<span className="fi fi-mk infoFlags"></span>
+				</Frame>
+				<p className="infoText">+38977926520</p>
+			</Fieldset>
+
+			<Fieldset legend="Email">
+				{/* mailto: link?? icon of mail? */}
+				<p className="infoText">elena_papazova@hotmail.com</p>
+			</Fieldset>
+
+			<Fieldset legend="Social Media">
+				<Frame className="languageFrame">{/* icon of linkedin/ etc.. */}</Frame>
+				<p className="infoText">elena.papazova</p>
+			</Fieldset>
+		</Frame>
+	);
 };
