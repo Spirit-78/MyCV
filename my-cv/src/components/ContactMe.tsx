3fa019a890f56865ed83c70f50cf66c6701a1fc0
ElenaPapazova <elena.papazova@miraclemill.com>
2022-05-09 20:40:20 +0200

    add icons

--- my-cv/src/components/ContactMe.tsx
+++ my-cv/src/components/ContactMe.tsx
@@ -1,23 +1,35 @@
 import { Fieldset, Frame } from "@react95/core";
+import { GrLinkedin } from "react-icons/gr";
+import { BsWhatsapp } from "react-icons/bs";
 
 export const ContactMe = () => {
 	return (
 		<Frame>
-			<Fieldset legend="Phone Number">
+			<Fieldset legend="Phone Number" className="fieldsetColumn">
 				<Frame className="languageFrame">
 					<span className="fi fi-mk infoFlags"></span>
 				</Frame>
 				<p className="infoText">+38977926520</p>
 			</Fieldset>
 
-			<Fieldset legend="Email">
+			<Fieldset legend="Email" className="fieldsetColumn">
 				{/* mailto: link?? icon of mail? */}
-				<p className="infoText">elena_papazova@hotmail.com</p>
+				<Frame className="languageFrame"></Frame>
+				<a
+					href="mailto:elena_papazova@hotmail.com"
+					className="infoText"
+					style={{ color: "blue" }}
+				>
+					elena_papazova@hotmail.com
+				</a>
 			</Fieldset>
 
-			<Fieldset legend="Social Media">
-				<Frame className="languageFrame">{/* icon of linkedin/ etc.. */}</Frame>
+			<Fieldset legend="Social Media" className="fieldsetColumn">
+				{/* icon of linkedin/ etc.. */}
+				<GrLinkedin size={25} />
 				<p className="infoText">elena.papazova</p>
+				<BsWhatsapp size={25} />
+				<p className="infoText">+38977926520</p>
 			</Fieldset>
 		</Frame>
 	);
